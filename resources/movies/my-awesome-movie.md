48734e8b1f1e56be3cd32ada0c82d6b838408388
Antonio J. Braun <antoniojbraun@gmail.com>
2024-07-25 20:56:36 -0300

    Primeiro commit

---
+++ resources/movies/my-awesome-movie.md
@@ -0,0 +1,13 @@
+---
+title: My Awesome Movie
+
+summary: Lorem ipsum dolor sit amet, dolore nulla eu ullamco magna dolor duis anim ex commodo in mollit ut.
+---
+Lorem ipsum dolor sit amet, dolore nulla eu ullamco magna dolor duis anim ex commodo in mollit ut.
+
+Ad commodo qui consectetur aliquip tempor.
+
+Voluptate commodo labore in pariatur enim excepteur occaecat reprehenderit eu labore voluptate velit
+sit est cupidatat amet minim aute.
+
+Lorem ullamco consequat eu nulla occaecat labore.
---
+++ start/routes.ts
@@ -0,0 +1,23 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+const RedisController = () => import('#controllers/redis_controller')
+import router from '@adonisjs/core/services/router'
+
+const MoviesController = () => import('#controllers/movies_controller')
+
+router.get('/', [MoviesController, 'index']).as('home')
+
+router
+  .get('/movies/:slug', [MoviesController, 'show'])
+  .as('movies.show')
+  .where('slug', router.matchers.slug())
+
+router.delete('/redis/flush', [RedisController, 'flush']).as('redis.flush')
+router.delete('/redis/:slug', [RedisController, 'destroy']).as('redis.destroy')
